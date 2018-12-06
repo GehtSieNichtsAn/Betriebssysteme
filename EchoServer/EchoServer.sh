ce8c72aa533e9fd55abfca38cd1c7c0f3f78ffb5
GehtSieNichtsAn <40341262+GehtSieNichtsAn@users.noreply.github.com>
2018-12-06 21:39:38 +0100

    Update EchoServer.sh

--- EchoServer/EchoServer.sh
+++ EchoServer/EchoServer.sh
@@ -4,8 +4,17 @@ receive() {
 	#request="$(nc -l -p 2020)"
 	#echo Received String from Client: $request
 
-	nc -l -p 2020 > out.txt	
-	cat out.txt
+	#nc -l -p 2020 > out.txt
+	
+	request="$(nc -l -p 2020)"
+	if grep -q $request out.txt; then
+    	echo found
+	else
+    	echo not found
+	fi
+	
+	echo -e "$request" >> out.txt
+	#cat out.txt
 }
 
 send() {
