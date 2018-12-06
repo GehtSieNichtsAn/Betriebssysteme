4ac723d29cdd41174aee0276ef3b51ea3aac9f94
GehtSieNichtsAn <40341262+GehtSieNichtsAn@users.noreply.github.com>
2018-12-06 21:39:09 +0100

    Update EchoClient.sh

--- EchoServer/EchoClient.sh
+++ EchoServer/EchoClient.sh
@@ -3,6 +3,27 @@ adresse=$1  #10.0.2.15
 port=$2
 text=$3
 
+send() {
+	echo -n $1 | nc -c $adresse $port
+}
+
+encrypt() {
+	pwd="$(echo -n $password | shasum -a 256)" #>> login.txt
+	echo $pwd
+}
+
+LoginRegister() {
+	echo -n "login: "
+	read loginname
+	send $loginname
+	echo "$loginname" >> login.txt
+	echo -n "password: "
+	read -s password
+	echo " "
+	encpwd=$(encrypt)
+	send $encpwd
+}
+
 #menu
 echo "======   Lab: Shell Programming (BS)   ======"
 echo 
@@ -15,14 +36,21 @@ read choice
 
 case "$choice" in
 	r) echo "----- R E G I S T E R -----"
-	   echo -n "login: "
-	   read loginname
-	   echo -n "password: "
-	   read -s password
-	   echo "$loginname" "$password" > login.txt
-	   echo "   ";;
+	   LoginRegister
+	   #echo -n "login: "
+	   #read loginname
+	   #echo -n "password: "
+	   #read -s password
+	   #encrypt
+	   #echo "$loginname" "$password" > login.txt
+	   #echo "   "
+	   ;;
 
-	l) echo "Login";;
+	l) echo "Login"
+	   echo -n 		
+
+
+;;
 	q) echo "Quit";;
 	*) echo "Invalid Command!";;
 esac
@@ -30,7 +58,9 @@ esac
 
 #send to server
 #echo -n $text | nc -c $adresse $port
-cat login.txt | nc -c $adresse $port
+#cat login.txt | nc -c $adresse $port
+#cat login.txt
+nc -c $adresse $port < login.txt
 echo $adresse, $port, $text	
 
 #receive from server
