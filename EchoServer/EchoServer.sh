7306325125d6222949b17fa5d1d9615d7b16d617
GehtSieNichtsAn <40341262+GehtSieNichtsAn@users.noreply.github.com>
2018-12-07 14:38:08 +0100

    Update EchoServer.sh

--- EchoServer/EchoServer.sh
+++ EchoServer/EchoServer.sh
@@ -1,30 +1,71 @@
 #!/bin/bash
-#var=$(nc -l -p 1337)
-receive() {
-	#request="$(nc -l -p 2020)"
-	#echo Received String from Client: $request
 
-	#nc -l -p 2020 > out.txt
+receive() {	
+	received="$(nc -l -p 2020)"
+	#if grep -q $request out.txt; then
+    #	echo found
+	#else
+    #	echo not found
+	#fi
 	
-	request="$(nc -l -p 2020)"
-	if grep -q $request out.txt; then
-    	echo found
-	else
-    	echo not found
-	fi
-	
-	echo -e "$request" >> out.txt
+	#echo -e "$request" >> out.txt
 	#cat out.txt
 }
 
 send() {
-	#echo -n $request | nc -c 10.0.2.15 2022
-	echo -n "Received" | nc -c 10.0.2.15 2022
+	echo -n $1 | nc -c 10.0.2.15 2022
 }
 
+loginClient() {
+	echo "Login..."
+	#Check Login
+	receive
+	read loginname < ./User
+	if [ "$loginname" = "$received" ]; then
+		echo "User exists"
+		send 1
+	else
+		echo "User doesnt exists"
+		send 0
+	fi
+	#Check Password
+	read C < ./C
+	send $C
+	receive
+	echo $received > ./tmpHashedPW
+	read hashedPW < ./HashedPW
+	tmphashedPW=`shasum -a 256 ./tmpHashedPW | cut -d ' ' -f1`
+	if [ "$hashedPW" = "$tmphashedPW" ]; then
+		echo "Password correct"
+		send 1
+	else
+		echo "Password incorrect"
+		send 0	
+	fi
+	#Check File
+	receive
+	send "$(cat "$received")"
+}
+
+registerClient() {
+	echo "Registration..."
+	receive
+	echo $received > ./User
+	receive
+	echo $received > ./HashedPW
+	receive
+	echo $received > ./N
+	echo 1 > ./C
+	send "Successful Registration!"
+}
 
 while true;
 do
 	receive	
-	send
+	case $received in
+		r) registerClient
+		;;
+		l) loginClient
+		;;
+	esac
 done
