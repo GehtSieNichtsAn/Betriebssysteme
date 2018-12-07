04b53cd55123124f7fff8648b78395344a3ff0bd
GehtSieNichtsAn <40341262+GehtSieNichtsAn@users.noreply.github.com>
2018-12-07 14:37:41 +0100

    Update EchoClient.sh

--- EchoServer/EchoClient.sh
+++ EchoServer/EchoClient.sh
@@ -1,30 +1,85 @@
 #!/bin/bash
-adresse=$1  #10.0.2.15
-port=$2
-text=$3
 
 send() {
-	echo -n $1 | nc -c $adresse $port
+	echo -n $1 | nc -c 10.0.2.15 2020
+}
+
+receive() {
+	received="$(nc -l -p 2022)"
 }
 
 encrypt() {
-	pwd="$(echo -n $password | shasum -a 256)" #>> login.txt
-	echo $pwd
+	hashedPW=`shasum -a 256 ./HashedPW | cut -d ' ' -f1` #mit Variable ersetzen
+	echo $hashedPW > ./HashedPW
 }
 
-LoginRegister() {
+loginAtServer() {
+	send l	
+	echo "----- L O G I N -----"
 	echo -n "login: "
 	read loginname
-	send $loginname
-	echo "$loginname" >> login.txt
 	echo -n "password: "
-	read -s password
+	read -s loginpassword
 	echo " "
-	encpwd=$(encrypt)
-	send $encpwd
+	echo -n "file name: "
+	read filePath
+	#Check Login
+	send $loginname
+	receive
+	if [ "$received" = "0" ]; then
+		echo "Invalid Username!"
+		exit -1
+	fi	
+	#Check Password
+	receive
+	tmpC=$received
+	read N < ./N
+	N=$((N - tmpC))
+	echo $loginpassword > ./tmpHashedPW
+	while [ $N -ne 0 ];
+	do
+		hashedPW=`shasum -a 256 ./tmpHashedPW | cut -d ' ' -f1`
+		echo $hashedPW > ./tmpHashedPW
+		N=$((N - 1))
+	done
+	send $hashedPW
+	receive
+	if [ "$received" = "0" ]; then
+		echo "Wrong Password!"
+		exit -1
+	fi
+	echo "Successful Login!"
+	#Check File
+	echo " "
+	send $filePath
+	receive	
+	echo "----- Requested File -----"
+	echo $received
 }
 
+registerAtServer() {
+	send r
+	echo "----- R E G I S T E R -----"
+	echo -n "login: "
+	read registername
+	send $registername
+	echo -n "password: "
+	read -s registerpassword
+	echo $registerpassword > ./HashedPW
+	for i in {0..9}; do
+		encrypt
+	done 
+	send $hashedPW
+	send 10
+	echo 10 > ./N
+	echo " "
+	receive
+	echo $received
+}
+
+while true;
 #menu
+echo " "
 echo "======   Lab: Shell Programming (BS)   ======"
 echo 
 echo "    r      Register"
@@ -34,35 +89,16 @@ echo
 echo -n "your choice: "
 read choice
 
-case "$choice" in
-	r) echo "----- R E G I S T E R -----"
-	   LoginRegister
-	   #echo -n "login: "
-	   #read loginname
-	   #echo -n "password: "
-	   #read -s password
-	   #encrypt
-	   #echo "$loginname" "$password" > login.txt
-	   #echo "   "
-	   ;;
-
-	l) echo "Login"
-	   echo -n 		
-
-
-;;
-	q) echo "Quit";;
-	*) echo "Invalid Command!";;
-esac
-
-
-#send to server
-#echo -n $text | nc -c $adresse $port
-#cat login.txt | nc -c $adresse $port
-#cat login.txt
-nc -c $adresse $port < login.txt
-echo $adresse, $port, $text	
-
-#receive from server
-request="$(nc -l -p 2022)"
-echo Received String from Server: $request
+do
+	case "$choice" in
+		r) registerAtServer	  
+		   ;;
+		l) loginAtServer
+    	   ;;
+		q) echo "Quit"
+		   exit 0
+		   ;;
+		*) echo "Invalid Command!"
+		   ;;
+	esac	
+done
